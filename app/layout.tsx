367c9cf3f6165e3feb69b4b8815ad62045823c8c
Ashish <90093173+ashurohilla@users.noreply.github.com>
2024-10-28 14:52:30 +0530

    ADD

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,52 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import { GeistSans } from 'geist/font/sans'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import Sessioprovider from '@/components/session-provider'
+import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import Navbar from '@/components/Navbar';
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : 'http://localhost:3000'
 
-import Navbar from "@/components/Navbar";
-const gelroy = localFont({
-  src: "./fonts/Gilroy-Regular.ttf",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-
-export const metadata: Metadata = {
-  title: "Home finder",
-  description: "find villas and property",
-};
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: 'Home finder',
+  description: 'The fastest way to find homes in Goa',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${gelroy.variable} ${gelroy.variable} antialiased`}
-      >
-
-        <div>
-
-        <Navbar/>
-        </div>
-        {children}
+    <html lang="en"  suppressHydrationWarning>
+      <meta name="google-adsense-account" content="ca-pub-7557474007097933">
+      </meta>
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7557474007097933"
+     crossOrigin="anonymous"></script>
+     <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+      </head>
+      <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className='w-full   mx-4'>
+              <Analytics/>
+              <SpeedInsights/>
+            </main>
+            <Navbar/>
+          {children}
+          </ThemeProvider>
+          <Toaster />
+          <Sessioprovider/>
       </body>
     </html>
-  );
-}
+  )
+} 
